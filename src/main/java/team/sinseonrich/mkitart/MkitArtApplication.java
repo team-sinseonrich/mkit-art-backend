c3f592696282a44bc3dc64e57b48d5ca596615d8
Sinseon Lee <sinseonky@gmail.com>
2023-06-25 22:07:10 +0900

    [MA-52] feat(*): Spring 프로젝트 추가
    - Spring initializer를 이용해 Project 생성함

---
+++ src/main/java/team/sinseonrich/mkitart/MkitArtApplication.java
@@ -0,0 +1,13 @@
+package team.sinseonrich.mkitart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkitArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkitArtApplication.class, args);
+	}
+
+}
